d3e87ba5f76da7118f65d8714f2378f68a3f0b63
k2core <7179062@gmail.com>
2022-03-02 00:18:39 +0900

    1.3 Routing 실패
    a 태그를 사용하면 리소스 리로드가 발생한다.
    import Link from "next/link" 사용

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Potato() {
-  return "about us";
+  return (
+    <nav>
+      <NavBar />
+      <h1>About</h1>
+    </nav>
+  );
 }
