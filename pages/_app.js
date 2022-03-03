6ac01a5d8f556c186cb3d2b8980cd331ffcb6ac8
k2core <7179062@gmail.com>
2022-03-03 19:30:52 +0900

    1.6 Custom APP#3
    index.js와 about.js에 공통으로 들어가는 NavBar 컴포넌트를 _app.js로 옮긴다.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <>
+      <NavBar />
       <Component {...pageProps} />
-      <span>hello</span>
-    </div>
+    </>
   );
 }
